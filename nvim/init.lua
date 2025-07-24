80d97f9c4206b77d33c60896040b726e873bdee4
Kadir Tastepe <70476992+kadirtastepe@users.noreply.github.com>
2025-07-24 09:33:56 +0200

    Add files via upload

---
+++ nvim/init.lua
@@ -0,0 +1,4 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
